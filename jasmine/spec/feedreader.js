b6ae64755e4d8e1cc0519903d2541a8d43a122e2
Robert Tulek <robert.tulek@gmail.com>
2019-04-21 13:28:52 +0200

    All required unit tests written in Jasmine. TODO: Write documentation.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -29,7 +29,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -44,7 +44,7 @@ $(function() {
 
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -74,7 +74,7 @@ $(function() {
 
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* Write a new test suite named "The menu" */
 
     describe('The menu', function() {
 
@@ -94,7 +94,7 @@ $(function() {
 
 
 
-        /* TODO: Write a test that ensures the menu changes
+        /* Write a test that ensures the menu changes
          * visibility when the menu icon is clicked. This test
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
@@ -119,32 +119,60 @@ $(function() {
 
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* Write a new test suite named "Initial Entries" */
 
     describe('Initial Entries', function() {
 
 
 
-
-        /* TODO: Write a test that ensures when the loadFeed
+        /* Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
+        beforeEach((done) => {  // fetch 'Udacity blog' RSS feed
+            loadFeed(0, done);
+        })
+
+        it('there is at least a single .entry element in .feed container mafter loadFeed function completes', function() {
+            expect($('.feed .entry').length).toBeGreaterThan(0); //Check if there is at least one entry inside feed-->entry
+        });
+
+
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* Write a new test suite named "New Feed Selection" */
 
     describe('New Feed Selection', function() {
 
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
 
+        let feedContentA, feedContentB;
+
+
+        // Fetch 2 (different) RSS feeds and store the HTML content in the feedContentA and feedContentB variables.
+        beforeEach((done) => {
+            loadFeed(1, function() {    // fetch 'CSS Tricks' RSS feed
+                feedContentA = $('.feed').html();
+                done();
+            })
+
+            loadFeed(2, function() {     // fetch 'HTML5 Rocks' RSS feed
+                feedContentB = $('.feed').html();
+                done();
+            })
+        });
+
+
+        it('content changes when a new feed is loaded by loadFeed function', function() {
+            expect(feedContentA).not.toBe(feedContentB);
+        });
 
     });
 
